f1c5f1efb813bed2e0944ced28eaedcfcb0a1662
devmdtanvir <officialtanvir@hotmail.com>
2023-01-03 15:05:23 +0600

    I am Update my Login.js file and folder

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,8 @@
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import React, { useState } from 'react';
+import { FcGoogle } from 'react-icons/fc';
 import app from '../../firebase.config';
+import img from '../../images/image.avif';
 import './Login.css';
 export const Login = () => {
 
@@ -8,12 +10,16 @@ export const Login = () => {
         name: '',
         email: '',
         password: '',
+        isLogin: false,
+        success: ''
     });
     const [newUser, setNewUser] = useState(false)
 
     console.log(newUser);
-    const handleSubmit = () => {
-        const auth = getAuth(app);
+    const auth = getAuth(app);
+
+
+    const handleSubmit = (e) => {
         if (newUser) {
             createUserWithEmailAndPassword(auth, user.email, user.password)
                 .then(res => {
@@ -26,16 +32,47 @@ export const Login = () => {
         if (!newUser) {
             signInWithEmailAndPassword(auth, user.email, user.password)
                 .then(res => {
-                    console.log(res);
+                    const newUser = { ...user };
+                    newUser.isLogin = true;
+                    setUser(newUser);
                 })
                 .catch((error) => {
                     const errorMessage = error.message;
-                    console.log(errorMessage);
+                    const newUsrInfo = { ...user };
+                    newUsrInfo.success = errorMessage;
+                    setUser(newUsrInfo);
+
                 });
         }
+        e.preventDefault()
     }
 
 
+    const googleProvider = new GoogleAuthProvider();
+    const googleSignIn = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(res => {
+                const { displayName, email } = res.user;
+                let newUsrInfo = { ...user };
+                newUsrInfo.name = displayName;
+                newUsrInfo.email = email;
+                setUser(newUsrInfo);
+            }).catch(error => {
+                console.log(error)
+            })
+    }
+
+    const signout = () => {
+        signOut(auth).then(() => {
+            const logOutUser = { ...user };
+            logOutUser.name = ''
+            logOutUser.email = ''
+            logOutUser.isLogin = true
+            setUser(logOutUser)
+        }).catch((error) => {
+            // An error happened.
+        });
+    }
 
     const handleBlur = e => {
         let isValid;
@@ -54,12 +91,25 @@ export const Login = () => {
     }
     return (
         <section className=' bg-gray-100 flex min-h-screen items-center justify-center'>
-            <div>
-                <div>
-                    <h1>hello world</h1>
+            <div className=' flex max-w-3xl rounded-lg p-20 bg-cyan-100 shadow-xl '>
+                <div className='md:w-1/2 px-16'>
+                    <h2 className=' text-xl text-cyan-400 font-semibold'>Login</h2>
+                    <p className=' text-cyan-300'>if you Already a Mumber easy login in </p>
+                    <label for="">New User</label>
+                    <input type="checkbox" name="" onChange={() => setNewUser(!newUser)} />
+                    <p>{user.name}</p>
+                    <form className=' grid grid-row-4 gap-6 mt-8'>
+                        {newUser && <input onBlur={handleBlur} className='p-2 rounded-xl outline-cyan-800' placeholder='Name' type="text" name="name" />}
+                        <input onBlur={handleBlur} className='p-2 rounded-xl outline-cyan-800' placeholder='Password' type="password" name="password" />
+                        <input onBlur={handleBlur} className='p-2 rounded-xl outline-cyan-800' placeholder='email' type="email" name="email" />
+                        <input onClick={handleSubmit} className=' p-2 bg-cyan-700 rounded-md text-white hover:scale-110 duration-500 cursor-pointer' type="submit" name="" value={user.isLogin ? 'LogOut' : 'Login'} />
+                    </form>
+                    {user.isLogin ? <button onClick={signout} className='hover:scale-110 duration-500 cursor-pointer mt-10 flex justify-center items-center py-2 bg-white rounded '><FcGoogle className=' mr-2' />Login out  Google</button> : <button onClick={googleSignIn} className='hover:scale-110 duration-500 cursor-pointer mt-10 flex justify-center items-center py-2 bg-white rounded '><FcGoogle className=' mr-2' />Login in Google</button>}
+
+                    <p className=' text-red-600'>{user.success}</p>
                 </div>
-                <div>
-                    <img src="../../images/image.avif" alt="" />
+                <div className='w-1/2 md:block hidden'>
+                    <img className=' rounded-md' src={img} alt="" />
                 </div>
             </div>
         </section>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,10 @@
 import React from 'react'
-
 export const Navigation = () => {
     return (
-        <div className=' flex justify-between items-center p-10 border-b border-teal-700 bg bg-gradient-to-tl dark:to-slate-900 dark:from-blue-800 bg'>
-            <div className='text-4xl font-extrabold'>Urban Tour</div>
-            <div className=' px-20 text-white'>
-                <a className=' px-14' href="/home">Home</a>
+        <div className=' flex justify-between items-center p-5 border-b bg-gradient-to-tl dark:to-slate-90'>
+            <div className='text-2xl font-extrabold'>Urban Tour</div>
+            <div className=' px-20'>
+                <a className='  hover:bg-gradient-to-br duration-100 from-indigo-900 to-violet-700  px-8 py-2 text-xl text-slate-800' href="/home">Home</a>
                 <a className=' px-14' href="/destination">Destination</a>
                 <a className=' px-14' href="/blog">Blog</a>
                 <a className=' px-14' href="/contact">Contact</a>
